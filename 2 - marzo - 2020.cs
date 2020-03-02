8443b01febe59271c854bcdd8f8b73d1003cf5f3
Samrod1603 <60189532+Samrod1603@users.noreply.github.com>
2020-03-02 12:51:39 -0500

    Update 2 - marzo - 2020.cs

--- 2 - marzo - 2020.cs
+++ 2 - marzo - 2020.cs
@@ -20,8 +20,8 @@ namespace Clase12Tarea
             Console.WriteLine("Ingrese la cantidad de datos a ingresar: ");
             int n = int.Parse(Console.ReadLine());
             string[] nombres = new string[n];
-            int[] puntajes = new int[n]; 
-            int puntajeMaximo = 0, puntajeMinimo = 100;
+            int[] puntajes = new int[n];
+            int puntajeMaximo = 0, puntajeMinimo = 100, segundo = 0, segundopuntaje = 0;
             string nombreMaximo = "nombre", nombreMinimo = "nombre";
             for (int f = 0; f < n; f++)
             {
@@ -31,13 +31,13 @@ namespace Clase12Tarea
                 nombres[f] = Console.ReadLine();
 
                 while (i < 1)
-            {
-                cartas = aleatorio.Next(1, 11);
-                total += cartas;
-                Console.WriteLine("esta fue su carta:" + cartas);
-                Console.WriteLine("esete es su puntaje:" + total);
-                i++;
-            }
+                {
+                    cartas = aleatorio.Next(1, 11);
+                    total += cartas;
+                    Console.WriteLine("esta fue su carta:" + cartas);
+                    Console.WriteLine("esete es su puntaje:" + total);
+                    i++;
+                }
                 while (continuar == "s" && total < 21)
                 {
                     cartas = aleatorio.Next(1, 11);
@@ -47,29 +47,46 @@ namespace Clase12Tarea
                     if (total > 21)
                     {
                         Console.WriteLine("valiste verga, perdiste: " + total + "puntos");
+                        total = 0; 
                     }
                     else
                     {
                         Console.WriteLine("desea continuar (s/n)");
                         continuar = Console.ReadLine();
                     }
+
                     puntajes[f] = total; //almacenar puntajes 
+
                 }
                 if (total > puntajeMaximo)
                 {
                     total = puntajeMaximo;
                     puntajeMaximo = puntajes[f];
-                    nombreMaximo = nombres[f]; 
+                    nombreMaximo = nombres[f];
                 }
                 if (total < puntajeMinimo)
                 {
-                    total = puntajeMinimo; 
+                    total = puntajeMinimo;
                     puntajeMinimo = puntajes[f];
-                    nombreMinimo = nombres[f]; 
+                    nombreMinimo = nombres[f];
                 }
+                for (int a = 0; a < n; a++)
+                {
+                    if (puntajes[a] > segundopuntaje && puntajes[a] < puntajeMaximo)
+                    {
+
+                        segundopuntaje = puntajes[a];
+                        segundo = a;
+
+                    }
+                }
+
+               
             }
             Console.WriteLine("el jugador con mayor puntaje fue: " + nombreMaximo + ", con" + puntajeMaximo + "...puntos");
+            Console.WriteLine("el jugador con segundo mayor puntaje fue: " + nombres[segundo]+ ", con" + segundopuntaje + "...puntos");
             Console.WriteLine("el jugador con menor puntaje fue: " + nombreMinimo + ", con" + puntajeMinimo + "...puntos");
         }
     }
 }
+
